e320977e71906229df77ea8c6df30b77d8be7798
screfy <screfy@pm.me>
2021-09-09 15:00:40 +0200

    feat: add heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Heading } from './components/Heading'
+
 export function App(): JSX.Element {
-  return <p>Hello world!</p>
+  return (
+    <div className="flex items-center min-h-screen px-6 md:px-64 bg-secondary text-primary-light">
+      <div className="flex flex-col max-w-[37rem]">
+        <Heading />
+      </div>
+    </div>
+  )
 }
---
+++ src/components/Heading.tsx
@@ -0,0 +1,8 @@
+export function Heading(): JSX.Element {
+  return (
+    <div className="flex flex-col space-y-1">
+      <h1 className="text-6xl text-primary">screfy</h1>
+      <h2 className="text-2xl text-secondary-light">Fullstack Web Developer</h2>
+    </div>
+  )
+}
