47cd98ec62ecdd4f9483bf6ce18f7db6a55c1490
screfy <screfy@pm.me>
2021-09-09 12:25:43 +0200

    chore: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{ts,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
