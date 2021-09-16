7acfbc1bef1b1d591fccda7b55e01bd5a0d1f43a
screfy <screfy@pm.me>
2021-09-16 12:49:09 +0200

    feat: add Heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx'
+
+export interface HeadingProps {
+  as: `h${1 | 2 | 3}`
+  text: string
+}
+
+enum FONT_SIZE {
+  h1 = 'text-3xl',
+  h2 = 'text-2xl',
+  h3 = 'text-base',
+}
+
+export default function Heading({ as, text }: HeadingProps): JSX.Element {
+  const Component = as
+
+  return <Component className={clsx('text-white font-semibold', FONT_SIZE[as])}>{text}</Component>
+}
