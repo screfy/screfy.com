9bee5a5795026cb15db2f6611eae47a8e53fc7d1
screfy <screfy@pm.me>
2021-09-16 17:20:36 +0200

    feat: add Section component

---
+++ src/components/Section.tsx
@@ -0,0 +1,16 @@
+import Heading, { HeadingProps } from './Heading'
+import { ReactChild } from 'react'
+
+interface Props {
+  heading: HeadingProps
+  children: ReactChild | ReactChild[]
+}
+
+export default function Section({ heading, children }: Props): JSX.Element {
+  return (
+    <section className="flex flex-col space-y-4">
+      <Heading {...heading} />
+      {children}
+    </section>
+  )
+}
