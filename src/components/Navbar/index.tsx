746132de0892fccad567346533532913343b4558
screfy <screfy@pm.me>
2021-09-17 01:05:18 +0200

    feat: add switching page sound

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,15 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import useSound from 'use-sound'
 import NavbarItem from './NavbarItem'
 
 export default function Navbar(): JSX.Element {
+  const { asPath } = useRouter()
+  const [play] = useSound('/beep.mp3')
+
+  useEffect(() => play(), [asPath])
+
   return (
     <nav className="flex items-center space-x-6 mb-10">
       <Link href="/" passHref>
