d9bdfabf1e56337c20c1e02d0a9a2de9e7536132
screfy <screfy@pm.me>
2021-09-16 13:11:06 +0200

    feat: add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import { GitHub, Twitter, Key } from 'react-feather'
+import FooterItem from './FooterItem'
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="flex ml-auto space-x-4">
+      <FooterItem href="https://github.com/screfy">
+        <GitHub size="20" />
+      </FooterItem>
+
+      <FooterItem href="https://twitter.com/screfy_">
+        <Twitter size="20" />
+      </FooterItem>
+
+      <FooterItem href="https://keybase.io/screfy">
+        <Key size="20" />
+      </FooterItem>
+    </footer>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { AppProps } from 'next/app'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import 'tailwindcss/tailwind.css'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <div className="flex min-h-screen bg-black text-primary">
-      <div className="max-w-3xl mx-auto py-8">
+      <div className="flex flex-col max-w-3xl mx-auto py-8">
         <Navbar />
-        <Component {...pageProps} />
+        <div className="flex-1">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
     </div>
   )
