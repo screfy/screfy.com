9228fd99dd387237290c845d6807bb763c058778
screfy <screfy@pm.me>
2021-09-17 00:36:29 +0200

    feat: add ProjectSkeleton component

---
+++ src/components/Project/ProjectSkeleton.tsx
@@ -0,0 +1,12 @@
+import { ChevronDown } from 'react-feather'
+
+export default function ProjectSkeleton(): JSX.Element {
+  return (
+    <div className="animate-pulse bg-secondary h-[42px] w-full rounded-lg">
+      <div className="h-full flex items-center justify-between px-4 py-2">
+        <div className="h-[14px] w-20 bg-[#5e5e5e] rounded"></div>
+        <ChevronDown className="text-[#5e5e5e]" size="18" />
+      </div>
+    </div>
+  )
+}
