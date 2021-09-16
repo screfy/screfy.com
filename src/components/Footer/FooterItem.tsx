48d0f17b11c7f1f9993f214a1b61bbe3e8ee0be5
screfy <screfy@pm.me>
2021-09-16 13:10:35 +0200

    feat: add FooterItem component

---
+++ src/components/Footer/FooterItem.tsx
@@ -0,0 +1,14 @@
+import { ReactChild } from 'react'
+
+interface Props {
+  href: string
+  children: ReactChild
+}
+
+export default function FooterItem({ href, children }: Props): JSX.Element {
+  return (
+    <a className="hover:text-white transition-colors" href={href} target="_blank" rel="noreferrer">
+      {children}
+    </a>
+  )
+}
