705ba9887b76f42e629563328448469072c09e3c
screfy <screfy@pm.me>
2021-09-17 10:15:18 +0200

    feat: add twitter meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
         ]}
         additionalLinkTags={[{ rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon' }]}
         openGraph={{ type: 'website', images: [{ url: `${URL}/logo_social-media.png` }] }}
+        twitter={{ handle: '@screfy_', cardType: 'summary_large_image' }}
       />
 
         <Navbar />
