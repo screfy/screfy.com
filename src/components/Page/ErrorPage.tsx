c782b767ce4301c647e464db776dd0ae4b9d14b5
screfy <screfy@pm.me>
2021-09-17 11:17:48 +0200

    feat: add ErrorPage component

---
+++ src/components/Page/ErrorPage.tsx
@@ -0,0 +1,21 @@
+import { NextSeo } from 'next-seo'
+import { ReactChild } from 'react'
+import Page from '.'
+import Section from '../Section'
+
+interface Props {
+  statusCode: number | string
+  children?: ReactChild | ReactChild[]
+}
+
+export default function ErrorPage({ statusCode, children }: Props): JSX.Element {
+  return (
+    <Page>
+      <NextSeo title={`Error ${statusCode}`} noindex nofollow />
+
+      <Section heading={{ as: 'h1', text: `${statusCode}` }}>
+        {children === undefined ? <p>Something went wrong.</p> : children}
+      </Section>
+    </Page>
+  )
+}
