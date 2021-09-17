e534083ceea7a6e6c419558c206c7e80d53bce54
screfy <screfy@pm.me>
2021-09-17 10:08:40 +0200

    feat: add custom title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from 'next/app'
+import { NextSeo } from 'next-seo'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import 'tailwindcss/tailwind.css'
 
+const URL = process.env.NEXT_PUBLIC_URL
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <div className="flex min-h-screen bg-black text-primary">
       <div className="flex flex-col max-w-3xl mx-auto py-8">
+      <NextSeo titleTemplate="%s | screfy" title="Home" />
+
         <Navbar />
         <div className="flex-1">
           <Component {...pageProps} />
