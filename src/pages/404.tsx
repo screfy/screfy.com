2471561c18b57cee5cfddab359f849877a4afbbd
screfy <screfy@pm.me>
2021-09-17 11:17:55 +0200

    feat: add custom 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import ErrorPage from '../components/Page/ErrorPage'
+
+export default function Error404(): JSX.Element {
+  return (
+    <ErrorPage statusCode="404">
+      <p>The requested URL does not exist on the server.</p>
+
+      <p className="text-sm">That&apos;s all I know.</p>
+    </ErrorPage>
+  )
+}
