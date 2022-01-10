95cfd19ea996164d1844c9cb485004de7dd493c1
screfy <screfy@pm.me>
2022-01-10 19:33:06 +0100

    chore: update commit types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['chore', 'feat', 'fix', 'test', 'docs', 'ci', 'style', 'refactor', 'release']],
-    'scope-enum': [2, 'always', ['deps', 'deps-dev']],
+    'type-enum': [
+      2,
+      'always',
+      ['chore', 'feat', 'fix', 'test', 'docs', 'ci', 'perf', 'deps', 'style', 'refactor', 'release'],
+    ],
+    'scope-enum': [2, 'always', []],
   },
 }
