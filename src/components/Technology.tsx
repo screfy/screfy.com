8698236b3abae7f06d0e21a2b7d5a27823fc04ad
screfy <screfy@pm.me>
2022-01-11 16:01:03 +0100

    refactor: change Technology styles

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -1,5 +1,4 @@
 import { ReactChild } from 'react';
-import Heading from './Heading';
 
 export interface TechnologyProps {
   name: string;
@@ -10,17 +9,17 @@ export interface TechnologyProps {
 
 export default function Technology({ name, description, icon, color }: TechnologyProps): JSX.Element {
   return (
-    <div className="h-[120px] lg:h-[100px] w-full flex items-center p-4 space-x-4 border border-secondary hover:bg-secondary rounded-lg transition-colors">
+    <div className="h-[120px] lg:h-[100px] w-full flex items-center p-4 space-x-4 hover:bg-[#0F1214] hover:scale-[1.02] border border-gray-200 rounded-lg transition-all">
       <div
-        className="h-[64px] w-[64px] flex flex-shrink-0 items-center justify-center rounded-xl"
+        className="h-[64px] w-[64px] flex flex-shrink-0 items-center justify-center text-white rounded-lg"
         style={{ backgroundColor: color }}
       >
         {icon}
       </div>
 
       <div className="flex flex-col">
-        <Heading as="h3" text={name} />
-        <p className="text-sm">{description}</p>
+        <h4 className="font-semibold text-gray-50">{name}</h4>
+        <p className="text-sm text-gray-100">{description}</p>
       </div>
     </div>
   );
