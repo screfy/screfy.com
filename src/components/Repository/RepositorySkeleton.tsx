94b21f4d7baaf3e715808ec18bdbb412ec5b482d
screfy <screfy@pm.me>
2022-01-11 22:05:59 +0100

    refactor: change RepositorySkeleton styles

--- src/components/Repository/RepositorySkeleton.tsx
+++ src/components/Repository/RepositorySkeleton.tsx
@@ -2,10 +2,10 @@ import { ChevronDown } from 'react-feather';
 
 export default function RepositorySkeleton(): JSX.Element {
   return (
-    <div className="animate-pulse bg-secondary h-[42px] w-full rounded-lg">
+    <div className="animate-pulse bg-gray-300 h-[42px] w-full rounded-lg">
       <div className="h-full flex items-center justify-between px-4 py-2">
-        <div className="h-[14px] w-20 bg-[#5e5e5e] rounded"></div>
-        <ChevronDown className="text-[#5e5e5e]" size="18" />
+        <div className="h-[14px] w-20 bg-gray-100 rounded"></div>
+        <ChevronDown className="text-gray-100" size="18" />
       </div>
     </div>
   );
