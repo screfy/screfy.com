7bb71366c8b82165faadc95c1f75adce77320f5f
screfy <screfy@pm.me>
2022-01-11 16:35:52 +0100

    refactor: change `&apos;` to `’`

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ export default function Error404(): JSX.Element {
     <ErrorPage statusCode="404">
       <p>The requested URL does not exist on the server.</p>
 
-      <p className="text-sm">That&apos;s all I know.</p>
+      <p className="text-sm">That’s all I know.</p>
     </ErrorPage>
   );
 }
