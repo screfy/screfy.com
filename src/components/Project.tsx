fa246f6708c128190302d7f5ab017ab6a42b9fe3
screfy <screfy@pm.me>
2022-01-11 17:35:47 +0100

    refactor: change Project styles

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Heading from './Heading';
 
 export interface ProjectProps {
   name: string;
@@ -16,22 +15,22 @@ export interface ProjectProps {
 export default function Project({ name, role, description, image, url }: ProjectProps): JSX.Element {
   return (
     <a
-      className="flex flex-col space-y-3 p-4 border border-secondary hover:bg-secondary rounded-lg transition-colors"
+      className="flex flex-col space-y-3 p-4 hover:bg-gray-300 border border-gray-200 rounded-lg transition-all"
       href={url}
       target="_blank"
       rel="noreferrer"
     >
       <div className="flex items-center space-x-4">
-        <Image src={image} height="64px" width="64px" className="rounded-xl" alt={`${name} Logo`} />
+        <Image src={image} height="64px" width="64px" className="rounded-lg" alt={`${name} Logo`} />
 
         <div className="flex flex-col">
-          <Heading as="h3" text={name} />
-          <p className="text-sm">{role}</p>
+          <h4 className="font-semibold text-white">{name}</h4>
+          <p className="text-sm text-gray-50">{role}</p>
         </div>
       </div>
 
       <div className="flex flex-col">
-        <Heading as="h4" text="About" />
+        <h5 className="font-semibold text-sm text-gray-50">About</h5>
         <p className="text-sm">{description}</p>
       </div>
     </a>
