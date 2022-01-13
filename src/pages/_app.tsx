497bfaf7dcd082e638e7b0191b00a6e50742941e
screfy <screfy@pm.me>
2022-01-13 13:19:40 +0100

    feat: improve metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,29 @@
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import 'tailwindcss/tailwind.css';
 
-const URL = process.env.NEXT_PUBLIC_URL;
+const BASE_URL = 'https://screfy.com';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const { pathname } = useRouter();
+
   return (
     <div className="flex justify-center min-h-screen bg-gray-400 text-gray-100">
       <NextSeo
         titleTemplate="%s – screfy"
         title="Home"
-        canonical={URL}
         description="Hey, I’m screfy, a fullstack web developer and open-source enthusiast."
+        canonical={BASE_URL + pathname}
         additionalMetaTags={[
           { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
           { name: 'author', content: 'screfy (https://screfy.com)' },
-          { name: 'url', content: URL },
+          { name: 'url', content: BASE_URL + pathname },
         ]}
         additionalLinkTags={[{ rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon' }]}
-        openGraph={{ type: 'website', images: [{ url: `${URL}/logo_social-media.png` }] }}
+        openGraph={{ type: 'website', images: [{ url: `${BASE_URL}/logo_social-media.png` }] }}
         twitter={{ handle: '@screfy_', cardType: 'summary_large_image' }}
       />
 
