96f7c45780edc53e53b93aee00ec66eb7fbc0306
screfy <screfy@pm.me>
2022-01-13 21:36:12 +0100

    chore: add basic headers

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ module.exports = {
   swcMinify: true,
   reactStrictMode: true,
   poweredByHeader: false,
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+        { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+        { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+        { key: 'X-DNS-Prefetch-Control', value: 'on' },
+        { key: 'X-Content-Type-Options', value: 'nosniff' },
+        { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+      ],
+    },
+  ],
   images: {
     domains: [
       // Spotify CDN:
