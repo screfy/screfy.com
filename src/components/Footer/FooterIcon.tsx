30312995752cae35eb1a621f1a58af61ab0daf41
screfy <screfy@pm.me>
2022-01-13 21:32:21 +0100

    feat: add `aria-label` attribute for footer link icons

--- src/components/Footer/FooterIcon.tsx
+++ src/components/Footer/FooterIcon.tsx
@@ -2,12 +2,13 @@ import { ReactChild } from 'react';
 
 interface Props {
   href: string;
+  label: string;
   children: ReactChild;
 }
 
-export default function FooterIcon({ href, children }: Props) {
+export default function FooterIcon({ href, label, children }: Props) {
   return (
-    <a className="hover:text-white transition-colors" href={href} target="_blank" rel="noreferrer">
+    <a className="hover:text-white transition-colors" href={href} aria-label={label} target="_blank" rel="noreferrer">
       {children}
     </a>
   );
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,15 +7,15 @@ export default function Footer() {
   return (
     <footer className="flex flex-col space-y-3 mt-24">
       <div className="flex space-x-3">
-        <FooterIcon href="https://github.com/screfy">
+        <FooterIcon href="https://github.com/screfy" label="GitHub">
           <GitHub size="20" />
         </FooterIcon>
 
-        <FooterIcon href="https://twitter.com/screfy_">
+        <FooterIcon href="https://twitter.com/screfy_" label="Twitter">
           <Twitter size="20" />
         </FooterIcon>
 
-        <FooterIcon href="https://keybase.io/screfy">
+        <FooterIcon href="https://keybase.io/screfy" label="Keybase">
           <Key size="20" />
         </FooterIcon>
       </div>
