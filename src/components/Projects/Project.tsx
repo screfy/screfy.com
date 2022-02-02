68280a8db77152f3d242da578039dd904337d84c
screfy <screfy@pm.me>
2022-02-02 14:37:55 +0100

    refactor: cleanup `Project` component

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { ProjectDocument } from '../../../.contentlayer/types';
+import Heading from '../Heading';
 
 export default function Project({
   name,
@@ -10,8 +11,10 @@ export default function Project({
 }: ProjectDocument) {
   return (
     <a
-      className="flex flex-col space-y-3 rounded-lg border border-gray-200 bg-white p-4 transition-transform hover:scale-[1.02] dark:border-gray-600 dark:bg-gray-800"
+      className="flex flex-col space-y-4 rounded-lg border border-gray-200 bg-white p-4 transition-transform hover:scale-[1.02] dark:border-gray-600 dark:bg-gray-800"
       href={url}
+      title={`Open ${name} project homepage`}
+      aria-label={`Open ${name} project homepage`}
       target="_blank"
       rel="noreferrer"
     >
@@ -25,15 +28,15 @@ export default function Project({
         />
 
         <div className="flex flex-col">
-          <h4 className="font-semibold text-black dark:text-white">{name}</h4>
+          <Heading as="h5">{name}</Heading>
+
           <p className="text-sm text-gray-600 dark:text-gray-100">{role}</p>
         </div>
       </div>
 
       <div className="flex flex-col">
-        <h5 className="text-sm font-semibold text-gray-700 dark:text-gray-100">
-          About
-        </h5>
+        <Heading as="h5">About</Heading>
+
         <p className="text-sm">{description}</p>
       </div>
     </a>
