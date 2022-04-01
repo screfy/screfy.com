b3deb383cf0f3508b1394186f6e01d265867e950
screfy <screfy@pm.me>
2022-04-01 16:35:22 +0200

    deps: bump lru-cache to 7.7.3

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -2,4 +2,4 @@ import LRUCache from 'lru-cache';
 
 // Cache items for 24 hours:
 export const CACHE_MAX_AGE = 86400000;
-export const cache = new LRUCache({ max: 1024 * 1024, maxAge: CACHE_MAX_AGE });
+export const cache = new LRUCache({ max: 1024 * 1024, ttl: CACHE_MAX_AGE });
