8c511a1562b2df053b5b7920ce07a4adf69c1202
screfy <screfy@pm.me>
2022-07-22 22:16:12 +0200

    Add radix ui colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+/* eslint @typescript-eslint/no-var-requires: off */
+const radixColors = require('@radix-ui/colors');
+
+const colors = Object.fromEntries(
+	Object.keys(radixColors)
+		.filter((color) => color.includes('Dark') && !color.includes('A'))
+		.map((color) => {
+			const colorName = color.replace('Dark', '');
+
+			const scales = Object.keys(radixColors[color]).map((scale) => {
+				const [step] = scale.match(/\d+/);
+
+				return [step, radixColors[color][scale]];
+			});
+
+			return [[colorName], Object.fromEntries(scales)];
+		})
+);
+
 /**
  * @type {import('tailwindcss').Config}
  **/
 module.exports = {
 	content: ['./src/**/*.{ts,tsx}'],
 	theme: {
-		extend: {}
+		colors
 	},
 	plugins: []
 };
