28bb002052d315138df024bcc8705ba068f7bb2f
screfy <screfy@pm.me>
2022-07-23 16:04:45 +0200

    Add home page about me section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,24 +21,32 @@ export default function Home() {
 	useEffect(() => setVisible(!inView), [inView]);
 
 	return (
-		<motion.div
-			ref={ref}
-			className="flex items-center justify-between"
-			variants={variants}
-			initial="initial"
-			animate={inView ? 'initial' : 'animate'}
-			transition={{ duration: 0.6, ease: 'easeInOut' }}
-		>
-			<div className="flex items-center space-x-4">
-				<div className="h-16 w-16 rounded-full bg-blue-9" />
+		<div className="space-y-10">
+			<motion.div
+				ref={ref}
+				className="flex items-center justify-between"
+				variants={variants}
+				initial="initial"
+				animate={inView ? 'initial' : 'animate'}
+				transition={{ duration: 0.6, ease: 'easeInOut' }}
+			>
+				<div className="flex items-center space-x-4">
+					<div className="h-16 w-16 rounded-full bg-blue-9" />
 
-				<div>
-					<h1 className="text-3xl font-bold">screfy</h1>
-					<p className="text-gray-11">Software Engineer</p>
+					<div>
+						<h1 className="text-3xl font-bold">screfy</h1>
+						<p className="text-gray-11">Software Engineer</p>
+					</div>
 				</div>
-			</div>
 
-			<NavbarLinks />
-		</motion.div>
+				<NavbarLinks />
+			</motion.div>
+
+			<p>
+				Welcome to my small corner of the internet. I'm screfy, and I'm a
+				self-taught software engineer interested in web and serverless
+				technologies and DevOps practices.
+			</p>
+		</div>
 	);
 }
