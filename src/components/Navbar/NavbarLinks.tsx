edbe3e9f2ef776492c6682c7ed5cf8a390d3d98d
screfy <screfy@pm.me>
2022-07-23 13:19:44 +0200

    Add base for home page

---
+++ src/components/Navbar/NavbarLinks.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import { ReactNode } from 'react';
+import { Annotation } from '../../icons/Annotation';
+import { UserCircle } from '../../icons/UserCircle';
+
+interface NavbarItemProps {
+	href: string;
+	icon: ReactNode;
+	label: string;
+}
+
+const items: NavbarItemProps[] = [
+	{
+		href: '/about',
+		icon: <UserCircle />,
+		label: 'About'
+	},
+	{
+		href: '/blog',
+		icon: <Annotation />,
+		label: 'Blog'
+	}
+];
+
+function NavbarItem({ href, icon, label }: NavbarItemProps) {
+	return (
+		<Link href={href}>
+			<a
+				className="rounded-xl bg-gray-3 p-2 text-gray-8 transition-colors hover:bg-gray-4 hover:text-gray-9"
+				aria-label={label}
+			>
+				{icon}
+			</a>
+		</Link>
+	);
+}
+
+export function NavbarLinks() {
+	return (
+		<div className="flex items-center space-x-3">
+			{items.map((props, i) => (
+				<NavbarItem key={i} {...props} />
+			))}
+		</div>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { NavbarLinks } from '../components/Navbar/NavbarLinks';
+
 export default function Home() {
-	return <h1 className="text-2xl">Hello world</h1>;
+	return (
+		<div className="flex min-h-screen flex-col items-center">
+			<main className="mt-36 w-full max-w-screen-sm flex-1">
+				<div className="flex items-center justify-between">
+					<div className="flex items-center space-x-4">
+						<div className="h-16 w-16 rounded-full bg-blue-9" />
+
+						<div>
+							<h1 className="text-3xl font-bold">screfy</h1>
+							<p className="text-gray-11">Software Engineer</p>
+						</div>
+					</div>
+
+					<NavbarLinks />
+				</div>
+			</main>
+		</div>
+	);
 }
