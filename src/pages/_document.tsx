1f8f6aea22b12db4fd039ea14d174090cb80b295
screfy <screfy@pm.me>
2022-07-24 18:15:50 +0200

    Add scroll padding and smooth scroll behaviour

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html className="[color-scheme:dark]" lang="en">
+		<Html className="scroll-pt-24 scroll-smooth [color-scheme:dark]" lang="en">
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link
