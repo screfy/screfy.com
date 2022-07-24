c2ef7e89297fb5be7e42db44f6253418a3b4812e
screfy <screfy@pm.me>
2022-07-24 15:40:08 +0200

    Set CSS color scheme to dark

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html lang="en">
+		<Html className="[color-scheme:dark]" lang="en">
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link
