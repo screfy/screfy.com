f8f4146767afe69782f690443ccfbba50cd753e7
screfy <screfy@pm.me>
2022-07-24 18:01:01 +0200

    Add background blur to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -58,7 +58,7 @@ export function Navbar() {
 					exit="initial"
 					transition={{ duration: 0.2, ease: 'easeInOut' }}
 				>
-					<div className="-mx-4 flex flex-1 items-center justify-between rounded-xl bg-gray-2 px-4 py-2 shadow-md">
+					<div className="-mx-4 flex flex-1 items-center justify-between rounded-xl bg-gray-3/60 px-4 py-2 shadow-md backdrop-blur">
 						<div className="flex items-center space-x-3 overflow-hidden">
 							<Link
 								className="h-10 w-10 rounded-full bg-blue-9"
