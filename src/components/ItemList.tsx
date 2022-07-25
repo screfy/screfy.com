5663d3edd276a9a97f4e6eb215fbcd0656553b1b
screfy <screfy@pm.me>
2022-07-25 22:11:30 +0200

    Remove arrow from ItemList

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import Image from 'next/image';
-import { ArrowTopRight } from '../icons/ArrowTopRight';
 
 interface ItemListProps {
 	items: ItemProps[];
@@ -27,7 +26,7 @@ function Item({
 }: ItemProps) {
 	return (
 		<a
-			className="group flex items-center space-x-4 transition-opacity hover:!opacity-100"
+			className="flex items-center space-x-4 transition-opacity hover:!opacity-100"
 			href={url}
 			target="_blank"
 			rel="noreferrer"
@@ -43,16 +42,7 @@ function Item({
 
 			<div className="flex w-full items-center justify-between border-b border-gray-6 py-5">
 				<div>
-					<div className="flex w-fit items-center space-x-1 font-medium">
-						<p>{title}</p>
-
-						<ArrowTopRight
-							width="14"
-							height="14"
-							className="text-gray-8 opacity-0 transition-opacity group-hover:opacity-100"
-						/>
-					</div>
-
+					<p>{title}</p>
 					<p className="text-base leading-none text-gray-11">{subtitle}</p>
 				</div>
 
