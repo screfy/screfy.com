2e157c7d673d7ab2e0403e8df86bb57a4487d3f7
screfy <screfy@pm.me>
2022-07-27 20:12:11 +0200

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,11 +1,46 @@
 /* eslint @typescript-eslint/no-var-requires: off */
 const { withContentlayer } = require('next-contentlayer');
 
+const securityHeaders = [
+	{
+		key: 'X-DNS-Prefetch-Control',
+		value: 'on'
+	},
+	{
+		key: 'Strict-Transport-Security',
+		value: 'max-age=31536000; includeSubDomains; preload'
+	},
+	{
+		key: 'X-Frame-Options',
+		value: 'DENY'
+	},
+	{
+		key: 'Permissions-Policy',
+		value: 'camera=(), microphone=(), geolocation=()'
+	},
+	{
+		key: 'X-Content-Type-Options',
+		value: 'nosniff'
+	},
+	{
+		key: 'Referrer-Policy',
+		value: 'origin-when-cross-origin'
+	}
+];
+
 module.exports = withContentlayer({
 	swcMinify: true,
 	reactStrictMode: true,
 	images: {
 		domains: ['s2.googleusercontent.com', 'i.scdn.co']
+	},
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: securityHeaders
+			}
+		];
 	},
 	experimental: {
 		legacyBrowsers: false,
