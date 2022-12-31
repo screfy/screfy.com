6213c0579686206ea58d12a2bc4cc37dce7f633a
screfy <screfy@pm.me>
2022-12-31 14:22:51 +0100

    Design page cleanup

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,11 +1,48 @@
+import { pick } from 'contentlayer/client';
 import { InferGetStaticPropsType } from 'next';
 import { NextSeo } from 'next-seo';
+import Image from 'next/image';
 import { allSites } from '../../.contentlayer/generated';
-import { ItemList, ItemProps } from '../components/ItemList';
 
-export default function Design({
-	sites,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+function Website({
+	title,
+	url,
+	createdAtHuman,
+	sanitizedUrl,
+	imageUrl,
+}: Props['sites'][number]) {
+	return (
+		<a
+			className="flex items-center space-x-4 transition-opacity hover:!opacity-100"
+			href={url}
+			target="_blank"
+			rel="noreferrer"
+		>
+			<Image
+				src={imageUrl}
+				width={32}
+				height={32}
+				quality={95}
+				alt={`${title} Image`}
+			/>
+
+			<div className="flex w-full items-center justify-between border-b border-gray-6 py-5">
+				<div>
+					<p className="text-base md:text-lg">{title}</p>
+					<p className="text-sm leading-none text-gray-11 md:text-base">
+						{sanitizedUrl}
+					</p>
+				</div>
+
+				<p className="text-sm text-gray-11 md:text-base">{createdAtHuman}</p>
+			</div>
+		</a>
+	);
+}
+
+export default function Design({ sites }: Props) {
 	return (
 		<div className="space-y-8">
 			<NextSeo title="Design" />
@@ -15,20 +52,18 @@ export default function Design({
 				<p>Below you can find a collection of sites I like.</p>
 			</div>
 
-			<ItemList items={sites} size={32} />
+			<div className="[&>a]:hover:opacity-50 [&>a:last-child>div]:border-none">
+				{sites.map((props, i) => (
+					<Website key={i} {...props} />
+				))}
+			</div>
 		</div>
 	);
 }
 
 export function getStaticProps() {
-	const sites: ItemProps[] = allSites.map(
-		({ title, url, sanitizedUrl, imageUrl, createdAtHuman }) => ({
-			title,
-			url,
-			imageUrl,
-			subtitle: sanitizedUrl,
-			right: createdAtHuman,
-		})
+	const sites = allSites.map((site) =>
+		pick(site, ['title', 'url', 'createdAtHuman', 'sanitizedUrl', 'imageUrl'])
 	);
 
 	return {
