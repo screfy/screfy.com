df2357e83f08106a2c69432b9dbf88a16cb50c8b
screfy <screfy@pm.me>
2024-11-10 22:02:36 +0100

    Set time to 12 hour format

--- src/app/_components/Time.tsx
+++ src/app/_components/Time.tsx
@@ -13,6 +13,7 @@ const timeFormatter = new Intl.DateTimeFormat(undefined, {
 	hour: '2-digit',
 	minute: '2-digit',
 	second: '2-digit',
+	hour12: true,
 	timeZone: WATCH_TIMEZONE,
 });
 
--- src/ui/IconLink.tsx
+++ src/ui/IconLink.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import type { Icon } from '@phosphor-icons/react';
 import Link from 'next/link';
 
